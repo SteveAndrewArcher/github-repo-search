20e03a3c136f196e8ff8ba4353dfb51400e6996c
Steve Archer <sarcher@mintel.com>
2020-11-29 10:15:46 -0600

    consistent indenting

--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -4,21 +4,19 @@ import { DEBOUNCE_DELAY } from "utils/constants.js"
 
 
 export default function DebounceInput(input) {
-  const [debouncedInput, setDebouncedInput] = useState(input);
+    const [debouncedInput, setDebouncedInput] = useState(input);
 
-  useEffect(
-    () => {
-      // wait to actually run the setter
-      const handler = setTimeout(() => {
-        setDebouncedInput(input);
-      }, DEBOUNCE_DELAY);
+    useEffect(() => {
+        // wait to actually run the setter
+        const handler = setTimeout(() => {
+            setDebouncedInput(input);
+        }, DEBOUNCE_DELAY);
 
-      // when input changes, clearTimeout and then start it over
-      return () => {
-        clearTimeout(handler);
-      };
-    }, [input]
-  );
+        // when input changes, clearTimeout and then start it over
+        return () => {
+            clearTimeout(handler);
+        };
+    }, [input]);
 
-  return debouncedInput;
+    return debouncedInput;
 }
